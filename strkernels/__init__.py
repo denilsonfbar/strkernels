7cd3b77374423285c17f07aac485dc83a6e9d392
Denilson Fagundes Barbosa <denilsonfbar@gmail.com>
2024-05-28 09:58:03 +0100

    Cleaning

--- strkernels/__init__.py
+++ strkernels/__init__.py
@@ -2,9 +2,9 @@
 # Author: Denilson Fagundes Barbosa, denilsonfbar@gmail.com
 
 
-from .single_positional_string_kernel import SinglePositionalStringKernel
 from .fixed_degree_string_kernel import FixedDegreeStringKernel
 from .subsequence_string_kernel import SubsequenceStringKernel
+
 
 __all__ = ['SinglePositionalStringKernel',
            'FixedDegreeStringKernel',
