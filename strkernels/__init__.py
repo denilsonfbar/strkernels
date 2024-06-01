0075dbfd8bf18b65a3e8490620a1dbd032377c4f
Denilson Fagundes Barbosa <denilsonfbar@gmail.com>
2024-06-01 13:57:52 +0100

    Added Locality improved

--- strkernels/__init__.py
+++ strkernels/__init__.py
@@ -3,9 +3,10 @@
 
 
 from .fixed_degree_string_kernel import FixedDegreeStringKernel
+from .locality_improved_string_kernel import LocalityImprovedStringKernel
 from .subsequence_string_kernel import SubsequenceStringKernel
 
 
-__all__ = ['SinglePositionalStringKernel',
-           'FixedDegreeStringKernel',
+__all__ = ['FixedDegreeStringKernel',
+           'LocalityImprovedStringKernel',
            'SubsequenceStringKernel']
--- strkernels/core/str_kernel.c
+++ strkernels/core/str_kernel.c
@@ -6,6 +6,7 @@ Author: Denilson Fagundes Barbosa, denilsonfbar@gmail.com
 #include <stdint.h>
 #include "str_kernel.h"
 #include "fixed_degree_sk.h"
+#include "locality_improved_sk.h"
 #include "subsequence_sk.h"
 
 double compute_kernel(char *str_a, char *str_b,
@@ -18,6 +19,13 @@ double compute_kernel(char *str_a, char *str_b,
         int32_t degree = (int32_t) param_1;
         return (double)compute_fixed_degree_sk(str_a, str_b, degree);
     } 
+    else if (strcmp(kernel_name, "LocalityImprovedStringKernel") == 0) 
+    {
+        int32_t length = (int32_t) param_1;
+        int32_t inner_degree = (int32_t) param_2;
+        int32_t outer_degree = (int32_t) param_3;
+        return compute_locality_improved_sk(str_a, str_b, length, inner_degree, outer_degree);
+    }    
     else if (strcmp(kernel_name, "SubsequenceStringKernel") == 0) 
     {
         int32_t maxlen = (int32_t) param_1;
