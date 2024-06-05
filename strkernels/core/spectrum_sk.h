4764234f4548d6fe293aa688c179aca155015df7
Denilson Fagundes Barbosa <denilsonfbar@gmail.com>
2024-06-05 11:01:26 +0100

    Adding spectrum kernel

---
+++ strkernels/core/spectrum_sk.h
@@ -0,0 +1,14 @@
+/*
+Author: Denilson Fagundes Barbosa, denilsonfbar@gmail.com
+*/
+
+#ifndef SPECTRUM_SK_H
+#define SPECTRUM_SK_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+double compute_spectrum_sk(char *str_a, char *str_b, 
+                           bool use_sign);
+
+#endif
